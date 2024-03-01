9b10395d7e927c9c8ced131f10b2b687bff080c5
01ou <141899225+01ou@users.noreply.github.com>
2024-03-01 15:29:12 +0900

    add migrations

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
-from backend.models.models import db
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 # アプリケーションの作成と構成
 def create_app():
@@ -9,18 +14,16 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'  # SQLite データベースの場合
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # Initialize SQLAlchemy database object
+    # SQLAlchemyの初期化
     db.init_app(app)
-
-    # APIルート（Blueprint）の登録
-    from backend.api import routes as api_routes
-    app.register_blueprint(api_routes.api)
-
-    # ミドルウェア関数の登録
-    from backend.middlewares import middleware_functions
-    # middleware_functionsを使用してミドルウェアを登録する
+    migrate.init_app(app, db, directory=os.path.join(os.path.dirname(__file__), 'migrations'))
 
     return app
+
+"""
+flask db migrate -m "マイグレーションの説明"
+flask db upgrade
+"""
 
 """
 Command to initialize the database.
